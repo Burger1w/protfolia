d9ddf49143554322fecf2a2bb7105094e0b7114d
Burger1w <104843190+Burger1w@users.noreply.github.com>
2022-10-09 12:47:39 +0300

    Update clqu.config.js

--- clqu.config.js
+++ clqu.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     name: 'clqu',
     version: '3.0.0',
-    githubName: 'clqu',
+    githubName: 'Burger1w',
     githubKey: process.env.GITHUB_KEY,
     softwareerKey: process.env.SOFTWAREER_KEY,
-    email: 'root@clqu.me',
+    email: 'info@kadybot.tk',
     errors: {
         404: "This page could not be found.",
         500: "An error occurred while processing your request."
